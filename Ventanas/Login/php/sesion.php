1d761b9049213c256439fb3a3b6b66cd68dbdb8b
DomenicaC <dvintimillac1@est.ups.edu.ec>
2021-11-24 16:36:55 -0500

    Conexion del login con la ventana principal

--- Ventanas/Login/php/sesion.php
+++ Ventanas/Login/php/sesion.php
@@ -11,7 +11,7 @@
     $result = $conn->query($sql);
     if ($result->num_rows > 0) {
         $_SESSION['isLogged'] = TRUE;
-        header("Location: ../../Principal/principal.php");
+        header("Location: ../../Principal/php/prin.html");
         echo "<p>ingreso!! :)</p>";
 
     } else {
