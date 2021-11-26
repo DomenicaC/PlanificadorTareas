90d16f2eee10ad5d4c19f44337b70a4b8e9a1a02
DomenicaC <dvintimillac1@est.ups.edu.ec>
2021-11-26 13:46:14 -0500

    pagina principal control de navegacion presentacion

--- Ventanas/Principal/mes/mes.php
+++ Ventanas/Principal/mes/mes.php
@@ -12,7 +12,7 @@ if (!isset($_SESSION['isLogged']) || $_SESSION['isLogged'] === FALSE) {
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="../../../css/princi.css">
+  <link rel="stylesheet" href="../../../css/mes.css">
   <script src="../js/calendario.js" type="text/javascript"></script>
   <title>Agenda Mes</title>
 </head>
--- Ventanas/Principal/php/principal.php
+++ Ventanas/Principal/php/principal.php
@@ -13,24 +13,62 @@ if (!isset($_SESSION['isLogged']) || $_SESSION['isLogged'] === FALSE) {
     <title>Pagina Principal</title>
 
     <link href="../../../css/princi.css" rel="stylesheet" />
+    <link href="../../../css/general.css" rel="stylesheet" />
 
 
 </head>
 
 <body>
-    <header>
-        <div class="nav-Primario">
-        <a href="../../../config/cerrarSesion.php">Cerrar Sesión</a>
 
-        </div>
+    <header class="enc1">
+        <img src="../../../images/iconos/calendar.png" alt="iconoLogo" />
+        <br />
+        <a class="cerrar" href="../../../config/cerrarSesion.php">Cerrar Sesión</a>
+        <h1>Planificador Empresarial</h1>
 
-        <div class="nav-Secundario">
-            <div class="principal"> <a href="../php/principal.php">Menú Principal</a> </div>
-            <div class="dia"> <a href="">Día</a> </div>
-            <div class="semana"> <a href="">Semana</a> </div>
-            <div class="mes"> <a href="../mes/mes.php">Mes</a> </div>
-        </div>
+
+        <br /> <br />
     </header>
+
+    <header class="tabla">
+        <br /> <br />
+        <nav>
+            <ul>
+                <li><a href="principal.php"> Página Principal </a></li>
+                <li><a href="../dia/dia.php"> Día </a> </li>
+                <li><a href="../semana/semana.php"> Semana </a></li>
+                <li><a href="../mes/mes.php"> Mes </a></li>
+            </ul>
+        </nav>
+        <br /> <br />
+    </header>
+
+    <section>
+        <h2>Seccion colaboradores</h2>
+        <h1>Colaboradores</h1>
+        <div class="colabs">
+            <div class="at"> <a href=""><img src="../../../images/iconos/ac.png" style="width: 2rem;" />Atención Cliente</a> </div>
+            <div class="ja"> <a href=""><img src="../../../images/iconos/ja.png" style="width: 2rem;" />Jefe Agencia</a> </div>
+            <div class="jt"> <a href=""><img src="../../../images/iconos/jt.png" style="width: 2rem;" />Jefe Tecnico</a> </div>
+            <div class="jv"> <a href=""><img src="../../../images/iconos/jv.png" style="width: 2rem;" />Jefe Ventas</a> </div>
+        </div>
+    </section>
+
+    <section>
+        <h2>Seccion de dos columnas</h2>
+        <img src="../../../images/diagrama/driagraPastel.jpeg" " />
+    </section>
+
+    <footer class=" pie">
+        <br />PLANIFICADORA EMPRESARIAL &#8226;Byron Alejandro Godoy Tenesaca &nbsp; 28201 &#8226;
+        <a href="telf: +9534058822"> (953) 405-8822</a> <br />
+        <a href="mailto: @est.ups.edu.ec">@est.ups.edu.ec</a>
+
+        <br />PLANIFICADORA EMPRESARIAL &#8226; Domenica Fernanda Vintimilla Canizares &nbsp; 28201 &#8226;
+        <a href="telf: +953968741628"> (953) 9687416282</a> <br />
+        <a href="mailto: canizaresdomenica4@gmail.com">canizaresdomenica4@gmail.com</a>
+        <br />© Todos los derechos reservados
+        </footer>
 
 </body>
 
